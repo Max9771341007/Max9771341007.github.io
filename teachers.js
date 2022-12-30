0906718faa8bd343a63b1b841f99e36519ab85fb
Max9771341007 <max9771341007@gmail.com>
2022-12-30 11:02:43 +0300

    sdfghjk

--- teachers.js
+++ teachers.js
@@ -1,13 +1,152 @@
 let teachers = [
     {
-        name: 'Мирианна',
-        surname: 'Иванова',
-        thirdName: 'Александровна',
-        class: '10А',
-        sience: 'Математика',
+        surname: 'Самоварова',
+        name: 'Лидия',
+        thirdName: 'Ивановна',
+        sience: 'История и обществознание',
+        specialization: 'Заведующая кафедрой.'
+    },
+    {
+        surname: 'Ставицкая ',
+        name: 'Елена ',
+        thirdName: 'Владимировна',
+        sience: 'История и обществознание ',
+        specialization: 'Учитель'
+    },
+    {
+        surname: 'Алиев',
+        name: 'Руслан',
+        thirdName: 'Гурбанович',
+        sience: 'География',
+        specialization: 'Учитель'
+    },
+    {
+        surname: 'Логвинова',
+        name: 'Любовь',
+        thirdName: 'Петровна',
+        sience: 'Истории и обществознания',
         specialization: 'учитель'
     },
     {
+        surname: 'Крылова ',
+        name: 'Валерия',
+        thirdName: ' Валерьевна',
+        sience: 'Английский язык',
+        specialization: 'Заведующая кафедрой'
+    },
+    {
+        surname: 'Карамышева ',
+        name: 'Марина ',
+        thirdName: 'Юрьевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    }, {
+        surname: 'Левина',
+        name: 'Мария',
+        thirdName: 'Алексеевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    }, {
+        surname: 'Сапожникова',
+        name: 'Анна',
+        thirdName: 'Владимировнаа',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    }, {
+        surname: 'Коломбет',
+        name: 'Татевик',
+        thirdName: 'Кареновна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    }, {
+        surname: 'Клепикова',
+        name: 'Юлиана',
+        thirdName: 'Валерьевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Проценко',
+        name: ' Светлана ',
+        thirdName: 'Николаевна',
+        sience: 'Французский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Семенова',
+        name: 'Елена',
+        thirdName: 'Владимировна',
+        sience: 'Немецкий язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Алексеева',
+        name: 'Валерия',
+        thirdName: ' Вячеславовна',
+        sience: 'Китайский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Словенко',
+        name: 'Алина ',
+        thirdName: 'Игоревна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Лаврина',
+        name: 'Анна ',
+        thirdName: 'Владимировна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Гусева ',
+        name: 'Ольга',
+        thirdName: ' Александровна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Корзинова ',
+        name: 'Ирина',
+        thirdName: 'Евгеньевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Пугачева ',
+        name: 'Ирина',
+        thirdName: 'Алексеевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Котляр ',
+        name: 'Ирина',
+        thirdName: ' Николаевна',
+        sience: 'Английский язык',
+        specialization: 'Учитель'
+    },{
+        surname: 'Гаврикова',
+        name: 'Ольга',
+        thirdName: 'Сергеевна',
+        sience: 'Математика',
+        specialization: 'Руководитель кафедры'
+    },
 
-    }
 ]
+let gridOption = document.querySelector('.teachers__grid-options');
+
+function teachersList() {
+
+    teachers.forEach(function (item) {
+
+        gridOption.insertAdjacentHTML('afterend', '<div class="teachers__grid-user">' +
+            '<div class="teachers__grid-item" datatype="surname"> ' +item.surname + '</div>' +
+            '<div class="teachers__grid-item" datatype="name"> ' +  item.name + '</div>' +
+            '<div class="teachers__grid-item" datatype="thirdname"> ' +   item.thirdName + '</div>' +
+            '<div class="teachers__grid-item" datatype="sience"> ' + item.sience +  '</div>' +
+            '<div class="teachers__grid-item" datatype="specialization">' + item.specialization +  '</div>' +
+            '</div>')
+    })
+
+}
+
+teachersList();
+
+
+// Самостоятельная работа
+// создать массив из учеников(3-5 учащихся) и вывести их на соответствующую страницу. Пункты таблицы и параметры продумать произвольно
+
--- uchen.js
+++ uchen.js
@@ -1,13 +1,45 @@
 let people = [
     {
         name: 'Самоходкин',
-        surname: '',
-        thirdName: '',
+        surname: 'afa',
+        thirdName: 'efrgfr',
         class: '10А',
-        sience: 'Математика',
-        specialization: 'учитель'
+        ruk:'Котляр'
+    },    {
+        name: 'Самоходкин',
+        surname: 'afa',
+        thirdName: 'efrgfr',
+        class: '10А',
+        ruk:'Котляр'
+    },    {
+        name: 'Самоходкин',
+        surname: 'afa',
+        thirdName: 'efrgfr',
+        class: '10А',
+        ruk:'Котляр'
+    },    {
+        name: 'Самоходкин',
+        surname: 'afa',
+        thirdName: 'efrgfr',
+        class: '10А',
+        ruk:'Котляр'
     },
-    {
-
-    }
 ]
+let gridpOption = document.querySelector('.classes__grid-options');
+
+function classesList() {
+
+    people.forEach(function (item) {
+
+        gridpOption.insertAdjacentHTML('afterend', '<div class="classes__grid-user">' +
+            '<div class="classes__grid-item" datatype="surname"> ' +item.surname + '</div>' +
+            '<div class="classes__grid-item" datatype="name"> ' +  item.name + '</div>' +
+            '<div class="classes__grid-item" datatype="thirdname"> ' +   item.thirdName + '</div>' +
+            '<div class="classes__grid-item" datatype="class"> ' +   item.class + '</div>'+
+        '<div class="classes__grid-item" datatype="ruk"> ' + item.ruk +  '</div>'+
+            '</div>')
+    })
+
+}
+
+classesList();
